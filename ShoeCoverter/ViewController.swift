91294e8314bd79661e2e303c23f4a7c7d37370a5
Chris Shaughnessy <chris@webifieddesign.com>
2014-11-14 15:31:08 -0500

    Initial Commit

---
+++ ShoeCoverter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShoeCoverter
+//
+//  Created by Chris Shaughnessy on 11/14/14.
+//  Copyright (c) 2014 Webified Design. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
